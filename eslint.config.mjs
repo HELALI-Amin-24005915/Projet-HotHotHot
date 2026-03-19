ec3602766d5226f7e9ffa417d20c22b43a4d82ab
HELALI <amin.helali@etu.univ-amu.fr>
2026-03-19 11:37:17 +0100

    chore: migration vers ESLint v10 (Flat Config)

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "prefer-const": "error",
+      "id-match": [
+        "error",
+        "^(?:[S]_.*|[I]_.*|[B]_.*|[F]_.*|[O]_.*|[A]_.*)$",
+        {
+          "properties": false,
+          "onlyDeclarations": true
+        }
+      ]
+    }
+  }
+];
